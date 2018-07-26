5dda1e7fad0f9b5ac430de263c105e6f1a7f8994
kritika <friendlykritika@gmail.com>
2018-07-26 12:37:44 +0530

    8

--- src/main/java/Lambdaparam.java
+++ src/main/java/Lambdaparam.java
@@ -2,7 +2,7 @@ public class Lambdaparam {
     public static void main(String[] args) {
         Addable ad = (a,b)->(a+b);
         System.out.println(ad.add(4,5));
-        Addable ad1 = (int a,int b)->(a+b);
+        Addable ad1 = (int a,int b)->{return(a+b);};
         System.out.println(ad1.add(10,20));
     }
 }
