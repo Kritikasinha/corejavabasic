f647b0ac43d29ad9611e880cdf0a9c69bcad8419
kritika <friendlykritika@gmail.com>
2018-07-26 12:31:48 +0530

    8

--- src/main/java/SayableLambdaEx.java
+++ src/main/java/SayableLambdaEx.java
@@ -1,8 +1,28 @@
-public class SayableLambdaEx {
+/*public class SayableLambdaEx {
     public static void main(String[] args) {
-        Sayablelambda s= ()->{
+   Sayablelambda s= ()->{
                 return  "I have nothing to say no parameters";
         };
         System.out.println(s.say());
+
+    }
+}*/
+
+
+public class SayableLambdaEx{
+    public static void main(String[] args) {
+
+        // Lambda expression with single parameter.
+        Sayablelambda s1=(name)->{
+            return "Hello, "+ name;
+        };
+        System.out.println(s1.say("KS"));
+
+        // You can omit function parentheses
+            Sayablelambda s2= name ->{
+                return "Hello, "+name;
+            };
+        System.out.println(s2.say("RK"));
     }
 }
+
