f15a0bd6c93645299982b75bbb3bc9f8c11b7b37
kritika <friendlykritika@gmail.com>
2018-07-26 12:35:58 +0530

    8

---
+++ src/main/java/Lambdaparam.java
@@ -0,0 +1,8 @@
+public class Lambdaparam {
+    public static void main(String[] args) {
+        Addable ad = (a,b)->(a+b);
+        System.out.println(ad.add(4,5));
+        Addable ad1 = (int a,int b)->(a+b);
+        System.out.println(ad1.add(10,20));
+    }
+}
--- src/main/java/Sayablelambda.java
+++ src/main/java/Sayablelambda.java
@@ -1,4 +1,4 @@
 @FunctionalInterface
 public interface Sayablelambda {
-    public String say();
+    public String say(String name);
 }
