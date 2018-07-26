748cbaab541aff7086ed62c67ca03a77add59b3b
kritika <friendlykritika@gmail.com>
2018-07-26 12:13:54 +0530

    8

---
+++ src/main/java/SayableLambdaEx.java
@@ -0,0 +1,8 @@
+public class SayableLambdaEx {
+    public static void main(String[] args) {
+        Sayablelambda s= ()->{
+                return  "I have nothing to say no parameters";
+        };
+        System.out.println(s.say());
+    }
+}
---
+++ src/main/java/Sayablelambda.java
@@ -0,0 +1,3 @@
+public interface Sayablelambda {
+    public String say();
+}
